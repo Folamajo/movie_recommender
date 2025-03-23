2b3d0242bd69c82c79ee3b5e2942e756c6e06b0a
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-03-23 08:25:56 +0000

    Got ratings of user similar to target

--- backend/load.py
+++ backend/load.py
@@ -18,19 +18,43 @@ data = data.groupby (["userId", "title"])["rating"].mean()
 data = data.reset_index() 
 
 #Create user-item matrix
-data = data.pivot(index="userId", columns="title", values="rating")
+user_title_matrix = data.pivot(index="userId", columns="title", values="rating")
 
 #Replace NaN values with zeros using Pandas fillna()
-data = data.fillna(0)
+data_fillna = user_title_matrix.fillna(0)
 # print(data.head(5))
 
 #Compute similarity between users using cosine similarity 
    #Import cosine_similarity from skleab.metrics.pairwise 
    #Pass value into cosine_similarity method
-data_cos_sim = cosine_similarity(data)
+data_cos_sim = cosine_similarity(data_fillna)
 # print(data_cos_sim)
-data = pd.DataFrame(data_cos_sim, index = ["userId", "userId"])
-print(data)
+data = pd.DataFrame(data_cos_sim)
+# print(data.head(5))
+
+
+# 4 — BUILDING RECOMMENDATION FUNCTION 
+# 4.1 — Get index of taget user we will be generating recommendations for. 
+target_user_index = 1
+# 4.2 — Retrieve similarity score between target user and alll other users
+   # .loc() lets us pick specific  rows in dataframe 
+   # Filter out the target index because it would bias the recommendation 
+   # We only want to base recommendations on what other similar users have like not what the user already rated.
+   # so we need to drop the row of the user
+
+all_rows = data.loc[target_user_index]
+similar_rows = all_rows.drop(target_user_index)
+# 4.3 — Users sorted by similarity score(descending order)
+   # We dont want all users we want only the most similar 
+   # grab top 5 users
+similar_rows = pd.DataFrame(similar_rows)
+sorted_desc = similar_rows.sort_values(by=1, ascending=False)
+top_five_similar = sorted_desc.nlargest(5, 1)
+# 4.4 — Retrieve Movie rating of Top Similar Users
+   # These are the users whose ratings we'll analyze to find movies our target user has no seen but might like 
+similar_user_rating = data_fillna.loc[top_five_similar.index]
+print(similar_user_rating)
+#choose user you want to
 
 # pivot_table = merged_df.pivot(index ="userId", columns="title", values="rating")
 # # # print(movies_df.head(5))
